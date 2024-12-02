68bb37ec0b5c1b5699644a8ca97cc8a5edb10457
Radu Darii <Radu.Darii@redsailtechnologies.com>
2024-12-02 15:14:54 +0100

    add test file and branch

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,1 +1,19 @@
-print("Hello World")
+import requests
+
+GITHUB_BASE_URL = 'https://api.github.com'
+user_name = 'radu0822'
+repo_name = 'test_repo'
+
+repository_url = f"{GITHUB_BASE_URL}/repos/{user_name}/{repo_name}/pulls"
+
+pat = "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"
+
+headers = {
+    'Authorization': f"Bearer {pat}" }
+
+response = requests.get(repository_url, headers=headers)
+if response.status_code == 200:
+    print(response.json())
+    print("Everything is working")
+else:
+    print("Not working")
