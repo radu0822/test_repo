d58498aebccff73a72a0d3e981b1ef4b0c371733
Radu Darii <Radu.Darii@redsailtechnologies.com>
2024-12-02 15:28:39 +0100

    add test file and branch

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -5,10 +5,10 @@ from const import GITHUB_BASE_URL, user_name, repo_name, pat
 repository_url = f"{GITHUB_BASE_URL}/repos/{user_name}/{repo_name}/pulls"
 
 
-headers = {
-    'Authorization': f"Bearer {pat}" }
+# headers = {
+#     'Authorization': f"Bearer {pat}" }
 
-response = requests.get(repository_url, headers=headers)
+response = requests.get(repository_url)
 if response.status_code == 200:
     print(response.json())
     print("Everything is working")
